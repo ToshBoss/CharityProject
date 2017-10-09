03565b5ca1766236c0108186cbab19e28b751275
iamanssd <samu3lkang@gmail.com>
2017-10-09 16:43:35 -0400

    Add files via upload

---
+++ CharityProject/README.md
@@ -0,0 +1,2 @@
+# CharityProject
+A JS Mining Website that gives funds to charity.
---
+++ CharityProject/functions.js
@@ -0,0 +1,5 @@
+//more styles coming soon
+
+function turnOffBlockers() {
+	alert("Please Turn Off Any Anti-Virus Or AdBlockers For Best Results");
+}
