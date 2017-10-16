b6728d3eba3567af6b3c73321a449ec7bedfa201
ToshBoss <mailtojoshuaphilip@gmail.com>
2017-10-16 15:33:57 -0400

    Redone Adblock Detector.

--- functions.js
+++ functions.js
@@ -1,7 +1,6 @@
 /*
 Created by Samuel Kang, Safi Patel, Joshua Philip
 */
-
-function alertBlockAdblock(){
-	alert("Please disable Adblock or any type of Antivirus for the Miner to work");
+function lonely{
+	console.log("Im a lonely piece of code :(")
 }
